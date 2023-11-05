d8a284f14f25c70ac4a6430620e1c2aa6291f6d2
William Whitehead <willclashtest@gmail.com>
2023-11-04 19:16:45 -0500

    added graph

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for
 import yfinance as yf
 import pandas as pd
 import calc_12_avg as calc
+import stock_graph
+from bokeh.embed import components
 
 app = Flask(__name__)
 
@@ -11,10 +13,23 @@ def index():
         ticker_symbol = request.form['ticker']
 
         # Calculate the 12-month averages
-        monthly_averages = calc.calc_12_avg(ticker_symbol)
+        try:
+            monthly_averages = calc.calc_12_avg(ticker_symbol)
+        except:
+            return render_template('index.html', error=True)
 
-        return render_template('result.html', ticker=ticker_symbol, averages=monthly_averages)
+        stock_plot = stock_graph.generate_graph(ticker_symbol)
+        script, div = components(stock_plot)
+
+        return render_template('result.html', ticker=ticker_symbol, averages=monthly_averages, script=script, div=div)
     
     return render_template('index.html')
+
+@app.route('/graph/<ticker>', methods=['GET'])
+def show_graph(ticker):
+    stock_plot = stock_graph.generate_graph(ticker)
+    script, div = components(stock_plot)
+    return render_template('graph.html', script=script, div=div)
+
 if __name__ == '__main__':
     app.run(debug=True)
--- stock_graph.py
+++ stock_graph.py
@@ -1,66 +1,42 @@
-from bokeh.plotting import figure, output_file, show
+from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource
-from bokeh.io import output_notebook
+from calc_12_avg import calc_12_avg
 from datetime import datetime
 
-# Data
-dates = [
-    "2022-11-30",
-    "2022-12-31",
-    "2023-01-31",
-    "2023-02-28",
-    "2023-03-31",
-    "2023-04-30",
-    "2023-05-31",
-    "2023-06-30",
-    "2023-07-31",
-    "2023-08-31",
-    "2023-09-30",
-    "2023-10-31",
-]
-values = [
-    147.40110778808594,
-    137.29092298235213,
-    135.20216789245606,
-    150.4717704371402,
-    154.54204260784647,
-    164.59555294639185,
-    172.2935333251953,
-    184.0348161969866,
-    192.1520217895508,
-    180.9973615563434,
-    177.00250091552735,
-    174.85428728376115,
-]
+def generate_graph(ticker):
+    averages = calc_12_avg(ticker)
+    
+    dates = []
+    values = []
 
-# Convert date strings to datetime objects
-dates = [datetime.strptime(date, "%Y-%m-%d") for date in dates]
+    for month, average in averages.items():
+        # Convert Pandas Timestamp to string
+        month_str = month.strftime("%Y-%m-%d")
+        dates.append(datetime.strptime(month_str, "%Y-%m-%d"))
+        values.append(average)
 
-# Create a ColumnDataSource
-source = ColumnDataSource(data={"dates": dates, "values": values})
+    # Create a ColumnDataSource
+    source = ColumnDataSource(data={"dates": dates, "values": values})
 
-# Create the plot
-output_file("stock_price_ticker.html", title="Stock Price Ticker")
+    # Create the plot
+    p = figure(
+        x_axis_label="Date",
+        y_axis_label="Price",
+        x_axis_type="datetime",
+        title=f"Stock Price Ticker for {ticker}",
+        width=800,
+        height=300,
+        toolbar_location=None,
+        background_fill_color="#f0f0f0",
+        border_fill_color="#ffffff",
+    )
 
-p = figure(
-    x_axis_label="Date",
-    y_axis_label="Price",
-    x_axis_type="datetime",
-    title="Stock Price Ticker",
-    width=800,
-    height=300,
-    toolbar_location=None,
-    background_fill_color="#f0f0f0",
-    border_fill_color="#ffffff",
-)
+    p.grid.grid_line_alpha = 0.6
+    p.grid.grid_line_dash = "dotted"
 
-p.grid.grid_line_alpha = 0.6
-p.grid.grid_line_dash = "dotted"
+    p.line("dates", "values", source=source, line_width=2, line_color="#007bff")
 
-p.line("dates", "values", source=source, line_width=2, line_color="#007bff")
+    # Remove the x-axis major label ticks
+    p.xaxis.major_label_overrides = {}
 
-# Remove the x-axis major label ticks
-p.xaxis.major_label_overrides = {}
-
-# Show the plot
-show(p)
+    return p  # Return the Bokeh plot object
