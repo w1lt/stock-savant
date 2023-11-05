aaf5ed57742275cd014335679393d3f6a137b0b8
William Whitehead <willclashtest@gmail.com>
2023-11-05 06:06:07 -0600

    hey

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, request
 import calc_12_avg as calc
 import news_sentiment as news
 import social_media_trends as social
-#import llama as llm
+from subprocess import Popen, PIPE
+
+program_path = "/"
 
 app = Flask(__name__)
 
@@ -19,11 +21,20 @@ def index():
         social_data = social.analyze_social_media_data(ticker_symbol) #takes in company stock name as string
         social_sentiment = social_data[0]
         social_posts = social_data[1]
+        
+        p = Popen([program_path], stdout=PIPE, stdin=PIPE)
+        prompt = "create an analysis of a stock based on the following information: " + f"ticker: {ticker_symbol}\n" + f"monthly averate change from past 12 months: {monthly_averages}\n" + f"news sentiment score out of 5: {news_sentiment}\n" + f"social sentiment score out of 5: {social_sentiment}. Say if it woul be a good investment or not."
+        p.stdin.write(bytes(f"{prompt}\n"))
+        p.stdin.flush()
+
+        result = p.stdout.readline().strip()
+
+        score = 1
 
         #llm_response = llm.gen_output(ticker_symbol)
     
 
-        return render_template('result.html', ticker=ticker_symbol, averages=monthly_averages, avg_number = monthly_averages_1_number, news_sentiment=news_sentiment, social_sentiment=social_sentiment, social_posts = social_posts)
+        return render_template('result.html', ticker=ticker_symbol, averages=monthly_averages, avg_number = monthly_averages_1_number, news_sentiment=news_sentiment, social_sentiment=social_sentiment, social_posts = social_posts, result=result, score=score)
     
     return render_template('index.html')
 
---
+++ static/loading.js
@@ -0,0 +1,19 @@
+/** @format */
+
+// Get references to DOM elements
+const loadButton = document.getElementById("loadButton");
+const loadingScreen = document.getElementById("loadingScreen");
+const content = document.getElementById("content");
+
+// Add a click event listener to the button
+loadButton.addEventListener("click", () => {
+  // Show the loading screen
+  loadingScreen.style.display = "flex";
+
+  // Simulate loading by delaying for a few seconds (you can replace this with actual loading logic)
+  setTimeout(() => {
+    // Hide the loading screen and display the content
+    loadingScreen.style.display = "none";
+    content.style.display = "block";
+  }, 3000); // Change the timeout duration as needed
+});
