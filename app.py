a242e35a19ba64e7aca565471c99c451400ba862
William Whitehead <willclashtest@gmail.com>
2023-11-04 21:43:38 -0500

    plug

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 import yfinance as yf
 import pandas as pd
 import calc_12_avg as calc
